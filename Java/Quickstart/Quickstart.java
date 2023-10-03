08050ebc18293279e28e5bf6db846e8d748d2260
Abdullahs1214 <112925374+Abdullahs1214@users.noreply.github.com>
2023-10-03 06:04:24 +0000

    "f"

--- Java/Quickstart/Quickstart.java
+++ Java/Quickstart/Quickstart.java
@@ -1,13 +1,9 @@
 package Quickstart;
 public class Quickstart{
-    public static boolean isEven(int i) { 
-    return (i % 2) == 1;
-}
-public static void main(String[] args) {
-// The following prints all even numbers between 1 and 10 
-for(int i = 1; i < 100 && isEven(i); i++)
+    
 
-    System.out.println(i);
+public static void main(String[] args) {
+    System.out.println("hello");
 }
 
     
