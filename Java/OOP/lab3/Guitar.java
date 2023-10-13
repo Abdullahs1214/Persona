54ff4033bed8f26c03d6747061a859cadeb7e2a8
Abdullahs1214 <abdullah.khan.sherwani@gmail.com>
2023-10-13 01:33:15 -0600

    "ece325 lab3"

--- Java/OOP/lab3/Guitar.java
+++ Java/OOP/lab3/Guitar.java
@@ -1,5 +1,13 @@
 package OOP.lab3;
 
 public class Guitar {
-    
+    Guitar guitar;
+
+    public Guitar(){
+        this.guitar = guitar;
+    }
+
+    public String toString(){
+    return guitar;
+}
 }
